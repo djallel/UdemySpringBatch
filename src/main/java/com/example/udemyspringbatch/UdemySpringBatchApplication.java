ec1037346facb4be7ebf8b356fa89ae82dd1b17e
TABET Gaël <gael.b.tabet@gmail.com>
2023-12-19 16:11:03 +0100

    Initial commit

---
+++ src/main/java/com/example/udemyspringbatch/UdemySpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.udemyspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemySpringBatchApplication.class, args);
+    }
+
+}
